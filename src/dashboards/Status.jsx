16ceb0e5752d061a5596d4b465c2663b363d610e
AiFSD <isaac.equss@gmail.com>
2024-10-08 09:33:07 -0400

    src folder added

--- src/dashboards/Status.jsx
+++ src/dashboards/Status.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom"; 
-import "../styles/status.css"; 
+import "/src/styles/status.css"; 
 
 const Status = () => {
   const [events, setEvents] = useState([]);
